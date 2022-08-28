70cb450a1cb62c0d1c874f62fff52fc57a19904e
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2022-08-28 13:51:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ function countDown() {
 
 function resetCountdown() {
   clearInterval(x);
-  count_down = new Date('08/29/2022 20:00:00').getTime();
+  count_down = new Date('09/09/2022 20:00:00').getTime();
   x = setInterval(() => countDown(), second);
 }
