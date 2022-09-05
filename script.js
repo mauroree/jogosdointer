dd9ab9b58a0a192ed338435b2632f5d4a47cfb33
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2022-09-05 19:13:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
-let count_down = new Date('09/10/2022 16:00:00').getTime();
+let count_down = new Date('09/10/2022 16:30:00').getTime();
 let x = setInterval(() => countDown(), second);
 
 function countDown() {
