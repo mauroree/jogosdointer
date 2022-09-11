b1d469e22ddb9f3c39f87251245409b5eb90ef0b
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2022-09-11 13:20:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ let x = setInterval(() => countDown(), second);
 function countDown() {
 
   let now = new Date(Date.now()).getTime();
-  
-  
   let diff = count_down - now;
 
   document.getElementById('days').innerText = Math.floor(diff / day);
   document.getElementById('hours').innerText = Math.floor(diff % day / hour);
   document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
   document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+
+  if (day == 0 && hour == 0 && minute == 0 && second == 0) {
+    
+    console.log("TESTE");
+    
+  }
 }
 
 
