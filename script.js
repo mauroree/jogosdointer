8982c6e607af7d33b7ce56237da264224eea3b65
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2022-10-14 18:33:02 -0300

    timer

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
-const message = "Jogo já começou.";
 
-let count_down = new Date('09/11/2022 13:25:00').getTime();
+let count_down = new Date('10/13/2022 16:05:10').getTime();
 let x = setInterval(() => countDown(), second);
 
 function countDown() {
-  
-    let now = new Date(Date.now()).getTime();
-    let diff = count_down - now;
-  
-    document.getElementById('days').innerText = Math.floor(diff / day);
-    document.getElementById('hours').innerText = Math.floor(diff % day / hour);
-    document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
-    document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+
+  let now = new Date(Date.now()).getTime();
+  let diferenca = count_down - now;
+
+  if (diferenca < 0) {
+
+    stop();
+
+  } else {
+
+    document.getElementById('days').innerText = Math.floor(diferenca / day);
+    document.getElementById('hours').innerText = Math.floor(diferenca % day / hour);
+    document.getElementById('minutes').innerText = Math.floor(diferenca % hour / minute);
+    document.getElementById('seconds').innerText = Math.floor(diferenca % minute / second);
 
   }
 
-function resetCountdown() {
-  clearInterval(x);
-  count_down = new Date('09/19/2022 13:26:00').getTime();
-  x = setInterval(() => countDown(), second);
 }
