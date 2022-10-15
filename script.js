50f8761a7ef59771f6307a5d797fe3a486b975fe
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2022-10-14 21:04:39 -0300

    melhorias no timer e aparecnia do site

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const seg = 1000; // 1000ms =  1seg
-const min = seg * 60; 
-const hora = min * 60; 
+const seg = 1000; // 1000ms = 1s
+const min = seg * 60;
+const hora = min * 60;
 const dia = hora * 24;
 
 let count_down = new Date('10/16/2022 18:00:00').getTime();
@@ -11,10 +11,13 @@ function countDown() {
   let now = new Date(Date.now()).getTime();
   let diferenca = count_down - now;
 
-  if (diferenca < 0) {
+  if (diferenca < 0) { //se for <0 zera a contagem
 
-    stop();
-   
+    document.getElementById('dias').innerText = 0;
+    document.getElementById('horas').innerText = 0;
+    document.getElementById('minutos').innerText = 0;
+    document.getElementById('segundos').innerText = 0;
+
   } else {
 
     document.getElementById('dias').innerText = Math.floor(diferenca / dia);
